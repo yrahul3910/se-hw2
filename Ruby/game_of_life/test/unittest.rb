5eb846729f0aa7231ec584a6184588a358c6d6a8
Shuzheng <shuzheng@Shuzhengs-MBP.attlocal.net>
2020-08-26 15:08:55 -0400

    introduced bugs to ruby file

--- Ruby/game_of_life/test/unittest.rb
+++ Ruby/game_of_life/test/unittest.rb
@@ -3,14 +3,14 @@ require_relative '../../../Ruby/game_of_life/src/GOL.rb'
 
 class GOLTest < Test::Unit::TestCase
   def test_small
-    game_of_life "small", 6, 10
+    game_of_life "small", 4, 10
   end
 
   def test_midium
-    game_of_life "medium", 12, 20
+    game_of_life "medium", 6, 20
   end
 
   def test_large
-    game_of_life "large", 18, 30
+    game_of_life "large", 8, 30
   end
 end
