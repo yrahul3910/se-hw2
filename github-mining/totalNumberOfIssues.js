e9e8210f1c4044f35e0903c04165f5c8c4c97615
Shuzheng Wang <43383742+wangdavid84@users.noreply.github.com>
2020-09-01 23:16:48 -0400

    Add files via upload

---
+++ github-mining/totalNumberOfIssues.js
@@ -0,0 +1,22 @@
+const { Octokit } = require("@octokit/core");
+const octokit = new Octokit({ auth: `20f3225869549c231e065983a1a6a28943695436` });
+
+(async () => {
+	let results = await octokit.request('GET /users/{owner}/repos',
+	{
+  		owner: 'yrahul3910',
+	});
+	results = results["data"];
+	let repos = results.map(x => x.name)
+	let count = 0;
+	for (const repo of repos) {
+		let result = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+			owner: 'yrahul3910',
+			repo
+        });
+        result = result["data"];
+        count = count + result.map(x => x.state).filter(x => x == "open").length;
+        // console.log(result.map(x => x.state).filter(x => x == "open").length);
+	}
+	console.log(count);
+})();
